f8989baf189ac36897ba4843fd10669c4f2f9156
DJ Quardaboff <Spartacrafter@gmail.com>
2018-02-05 16:12:28 -0600

    use only 3rd name in packageId instead of whole packageId

--- app/src/main/java/com/porterlee/standardinventory/InventoryActivity.java
+++ app/src/main/java/com/porterlee/standardinventory/InventoryActivity.java
@@ -1101,7 +1101,7 @@ public class InventoryActivity extends AppCompatActivity implements ActivityComp
                 //int locationIndex = 0;
 
                 //
-                String tempText = BuildConfig.APPLICATION_ID + "|" + BuildConfig.BUILD_TYPE + "|v" + BuildConfig.VERSION_NAME + "|" + BuildConfig.VERSION_CODE + "\r\n";
+                String tempText = BuildConfig.APPLICATION_ID.split(".")[2] + "|" + BuildConfig.BUILD_TYPE + "|v" + BuildConfig.VERSION_NAME + "|" + BuildConfig.VERSION_CODE + "\r\n";
 
                 printStream.print(tempText);
                 printStream.flush();
